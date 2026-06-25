285a919026603641bd0277f5a3e32c7fe52a9327
mbarkiiiiiiiiiiii <bachklitbachklit@gmail.com>
2026-06-25 13:29:55 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const cors = require('cors');
+const app = express();
 const path = require('path');
 
-const app = express();
-app.use(cors());
 app.use(express.json());
 
+// التعديل هنا: استخدام __dirname باش يلقى index.html فين ما كان
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// باقي الكود ديالك...
 // البيانات في الذاكرة (مؤقتة)
 let database = { users: [], decisions: [] };
 
